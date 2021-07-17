805a513ddbc352fe2f424e538206944948cd7102
makan-dianka <python3.230492@gmail.com>
2021-07-17 15:18:30 +0200

    script pour faire de recherche sur google via la console

--- google/google.py
+++ google/google.py
@@ -3,7 +3,7 @@ import time
 import sys
 from bs4 import BeautifulSoup
 
-page = 0
+line = 0
 
 
 def google_research(url):
@@ -33,11 +33,11 @@ class_principal7 = google.find_all('div', class_='ZINbbc xpd O9g5cc uUPGi')[10:1
 print(f"\n\t\tgoogle results for : {keyword}".upper())
 
 def google_resultats():
-    global page
+    global line
     for i in class_principal:
         for p in i.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
-            page += 1
-            print("GOOGLE Line "+str([page]))
+            line += 1
+            print("GOOGLE Line "+str([line]))
             try:
                 links = i.a["href"]
                 link_sa = links.split("=")
@@ -57,11 +57,11 @@ google_resultats()
 
 def google_resultats1(s):
     if s.lower() == "":
-        global page
+        global line
         for i in class_principal1:
             for p in i.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
-                page += 1
-                print("GOOGLE Line "+str([page]))
+                line += 1
+                print("GOOGLE Line "+str([line]))
                 try:
                     links = i.a["href"]
                     link_sa = links.split("=")
@@ -88,7 +88,7 @@ def google_resultats1(s):
         except AttributeError:
             print("invalid url".upper())
         
-print("Page suivante\nCliquez sur le boutton [entrer] pour la page suivante - OU - \nCliquez sur n'import quel autre boutton pour suivre un lien\nCliquez q, z, c pour quiter")
+print("Page suivante\nCliquez sur le boutton [entrer] pour la line suivante - OU - \nCliquez sur n'import quel autre boutton pour suivre un lien\nCliquez q, z, c pour quiter")
 suivant = input()
 if suivant.lower() in ["q", "z", "c"]:
     sys.exit()
@@ -97,11 +97,11 @@ google_resultats1(suivant)
 
 def google_resultats2(s):
     if s.lower() == "":
-        global page
+        global line
         for i in class_principal2:
             for p in i.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
-                page += 1
-                print("GOOGLE Line "+str([page]))
+                line += 1
+                print("GOOGLE Line "+str([line]))
                 try:
                     links = i.a["href"]
                     link_sa = links.split("=")
@@ -135,11 +135,11 @@ google_resultats2(suivant)
 
 def google_resultats3(s):
     if s.lower() == "":
-        global page
+        global line
         for i in class_principal3:
             for p in i.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
-                page += 1
-                print("GOOGLE Line "+str([page]))
+                line += 1
+                print("GOOGLE Line "+str([line]))
                 try:
                     links = i.a["href"]
                     link_sa = links.split("=")
@@ -173,11 +173,11 @@ google_resultats3(suivant)
 
 def google_resultats4(s):
     if s.lower() == "":
-        global page
+        global line
         for i in class_principal4:
             for p in i.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
-                page += 1
-                print("GOOGLE Line "+str([page]))
+                line += 1
+                print("GOOGLE Line "+str([line]))
                 try:
                     links = i.a["href"]
                     link_sa = links.split("=")
@@ -211,11 +211,11 @@ google_resultats4(suivant)
 
 def google_resultats5(s):
     if s.lower() == "":
-        global page
+        global line
         for i in class_principal5:
             for p in i.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
-                page += 1
-                print("GOOGLE Line "+str([page]))
+                line += 1
+                print("GOOGLE Line "+str([line]))
                 try:
                     links = i.a["href"]
                     link_sa = links.split("=")
@@ -248,11 +248,11 @@ google_resultats5(suivant)
 
 def google_resultats6(s):
     if s.lower() == "":
-        global page
+        global line
         for i in class_principal6:
             for p in i.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
-                page += 1
-                print("GOOGLE Line "+str([page]))
+                line += 1
+                print("GOOGLE Line "+str([line]))
                 try:
                     links = i.a["href"]
                     link_sa = links.split("=")
@@ -285,11 +285,11 @@ google_resultats6(suivant)
 
 def google_resultats7(s):
     if s.lower() == "":
-        global page
+        global line
         for i in class_principal7:
             for p in i.find_all("div", class_="BNeawe s3v9rd AP7Wnd"):
-                page += 1
-                print("GOOGLE Line "+str([page]))
+                line += 1
+                print("GOOGLE Line "+str([line]))
                 try:
                     links = i.a["href"]
                     link_sa = links.split("=")
