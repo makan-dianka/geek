893b02663e0fbb89e5a0e5495cb5b53680fada0b
makan-dianka <python3.230492@gmail.com>
2021-07-17 15:08:13 +0200

    envoyer un mail via python script

---
+++ mailing/mail.py
@@ -0,0 +1,27 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def mailing(sender, pwd, recever):
+    """
+    mailing prend 3 args pour envoyer un mail.
+    sender : mail de l'expéditeur
+    pwd    : mdp de l'expéditeur
+    recever: mail du destinateur
+    """
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(sender, pwd)
+    print("login success !")
+    
+    mail=MIMEMultipart()
+    mail['From'] = sender
+    mail['To'] = recever
+    mail['Subject'] = "Objet de votre e-mail"
+    message = "votre message ici"
+
+    mail.attach(MIMEText(message,'plain'))
+    text = mail.as_string()
+    server.sendmail(sender,recever,text)
+    print("le msg a été envoyé à", recever)
+    server.quit()
