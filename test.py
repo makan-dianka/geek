f3d017d49c2801574da97279717f121cb995c5ff
Makan Dianka <makan.dianka@hotmail.com>
2021-10-02 17:27:41 +0200

    add func

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-def cal(n):
-    return n**2
+import os
+
+user = os.getlogin()
+path = "c:/users/"+f"{user}"
+
+recherche = ["finance", "portofolio", "dagakane", "pro"]
+
+def verification(path):
+    listdir = os.listdir(path)
+    dossiers = [d for d in listdir if d.lower() in recherche]
+            
+    return dossiers
+    
+print(verification(path))
